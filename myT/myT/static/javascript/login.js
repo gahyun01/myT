1a0c6a31570dfefa4df42b6311140f6678bf4591
gahyun <gahyun727301@kangnam.ac.kr>
2024-11-21 18:01:17 +0900

    [Fix] Updated code to ensure successful login

--- myT/myT/static/javascript/login.js
+++ myT/myT/static/javascript/login.js
@@ -36,51 +36,92 @@ function checkAll(){
 	return ok;				
 }
 
+// 쿠키를 설정하는 함수
+function setCookie(name, value, days) {
+   var expires = "";
+   if (days) {
+       var date = new Date();
+       date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));  // 기간(days) 설정
+       expires = "; expires=" + date.toUTCString();
+   }
+   document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// 쿠키를 읽는 함수
+function getCookie(name) {
+   var nameEQ = name + "=";
+   var ca = document.cookie.split(';');
+   for (var i = 0; i < ca.length; i++) {
+       var c = ca[i].trim();
+       if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+   }
+   return null;
+}
+
+// 쿠키를 삭제하는 함수
+function eraseCookie(name) {
+   document.cookie = name + "=; Max-Age=-99999999; path=/";
+}
+
+// 초기 상태 읽기
+var id = getCookie('idChecked') === 'true' ? 'true' : 'false';
+var stay = getCookie('stayChecked') === 'true' ? 'true' : 'false';
+
+// 초기 이미지 설정 (쿠키 상태에 맞게)
+if (id === 'true') {
+   $("body form .lpage .main .check .idcheck img").css({ 'opacity': '1' });
+   $("body form .lpage .main .check .idcheck img").attr("src", logcheckcImg);
+}
+
+if (stay === 'true') {
+   $("body form .lpage .main .check .logcheck img").css({ 'opacity': '1' });
+   $("body form .lpage .main .check .logcheck img").attr("src", logcheckcImg);
+}
+
+// 아이디 저장 체크 버튼 클릭시
+$("body form .lpage .main .check .idcheck").click(function () {
+   if (id === 'false') {
+       $(this).find("img").css({ 'opacity': '1' });
+       $(this).find("img").attr("src", logcheckcImg);
+       id = 'true';
+       setCookie('idChecked', 'true', 7);  // 7일 동안 쿠키 유지
+   } else {
+       $(this).find("img").css({ 'opacity': '0.5' });
+       $(this).find("img").attr("src", logcheckImg);
+       id = 'false';
+       eraseCookie('idChecked');
+   }
+});
+
+// 로그인 상태 유지 체크 버튼 클릭시
+$("body form .lpage .main .check .logcheck").click(function () {
+   if (stay === 'false') {
+       $(this).find("img").css({ 'opacity': '1' });
+       $(this).find("img").attr("src", logcheckcImg);
+       stay = 'true';
+       setCookie('stayChecked', 'true', 7);  // 7일 동안 쿠키 유지
+   } else {
+       $(this).find("img").css({ 'opacity': '0.5' });
+       $(this).find("img").attr("src", logcheckImg);
+       stay = 'false';
+       eraseCookie('stayChecked');
+   }
+});
+
 $(document).ready(function () {
-    // 아이디 클릭시 이미지 변경
-    //$("body form .lpage .main .id").click(function () {
-    //    $(this).find("img").css({ 'opacity': '1' });
-    //});
-    //$("body form .lpage .main .id").focusout(function () {
-    //    $(this).find("img").css({ 'opacity': '0.5' });
-    //});
+   // 아이디 클릭시 이미지 변경
+   $("body form .lpage .main .id").click(function () {
+      $(this).find("img").css({ 'opacity': '1' });
+   });
+   $("body form .lpage .main .id").focusout(function () {
+      $(this).find("img").css({ 'opacity': '0.5' });
+   });
 
-    // 비밀번호 클릭시 이미지 변경
-    //$("body form .lpage .main .pwd").click(function () {
-    //    $(this).find("img").css({ 'opacity': '1' });
-    //});
-    //$("body form .lpage .main .pwd").focusout(function () {
-    //    $(this).find("img").css({ 'opacity': '0.5' });
-    //});
-
-    // 체크 버튼 클릭시
-    var logcheckImg = "{% static 'images/loglock.png' %}";
-    var logcheckcImg = "{% static 'images/loglockc.png' %}"; 
-    var id = 'false';
-    $("body form .lpage .main .check .idcheck").click(function () {
-       if(id === 'false'){
-        $(this).find("img").css({ 'opacity': '1' });
-        $(this).find("img").attr("src", logcheckcImg);
-        id = 'true';
-       }
-       else{
-        $(this).find("img").css({ 'opacity': '0.5' });
-        $(this).find("img").attr("src", logcheckImg);
-        id = 'false';
-      }
-    });
-
-    var stay = 'false';
-    $("body form .lpage .main .check .logcheck").click(function () {
-       if(stay === 'false'){
-        $(this).find("img").css({ 'opacity': '1' });
-        $(this).find("img").attr("src", logcheckcImg);
-        stay = 'true';
-       }
-       else{
-        $(this).find("img").css({ 'opacity': '0.5' });
-        $(this).find("img").attr("src", logcheckImg);
-        stay = 'false';
-       }
-    });    
+   // 비밀번호 클릭시 이미지 변경
+   $("body form .lpage .main .pwd").click(function () {
+      $(this).find("img").css({ 'opacity': '1' });
+   });
+   $("body form .lpage .main .pwd").focusout(function () {
+      $(this).find("img").css({ 'opacity': '0.5' });
+   });
 });
--- myT/myT/urls.py
+++ myT/myT/urls.py
@@ -3,6 +3,7 @@ from . import views
 from myT.views import logout_view
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),  # 기본 경로로 views.index 뷰를 처리
@@ -10,7 +11,7 @@ urlpatterns = [
 
 # 로그인 및 로그아웃 URL
 urlpatterns += [
-    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', logout_view, name='logout'),
 ]
 
